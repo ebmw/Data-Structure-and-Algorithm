10fd70388a7e423cdc9ed2917e8c871627ad8be9
ebmw <khentacheabderaouf@gmail.com>
2025-01-15 11:38:15 +0100

    to try pull request

--- Data Structures/Stacks/stack.cpp
+++ Data Structures/Stacks/stack.cpp
@@ -1,38 +1,93 @@
 #include <iostream>
-using namespace std; 
+#include <assert.h>
+using namespace std;
+
 template <typename T>
-class Stack {
+class stack
+{
 private:
-    struct Node {
+    struct Node
+    {
         T data;
         Node *next;
-        Node(const T &data, Node *next = nullptr) : data(data), next(next) {}
+
+        Node(T value, Node *nextptr = nullptr) : data(value), next(nextptr) {}
     };
 
-    Node *topNode;
+    Node *topnode;
 
 public:
-    Stack() : topNode(nullptr) {}
+    stack() : topnode(nullptr) {}
     
-    bool isEmpty() const { return topNode == nullptr; }
-
-    void push(const T &x) {
-        topNode = new Node(x, topNode);
+    bool isEmpty() const { return topnode == nullptr; }
+    
+    T top() const
+    {
+        if (isEmpty())
+            assert(false && "Stack Is Empty");
+        return topnode->data;
     }
-
-    void pop() {
-        if (isEmpty()) return;
-        Node *oldTop = topNode;
-        topNode = topNode->next;
-        delete oldTop;
+    
+    void Push(T value)
+    {
+        topnode = new Node(value, topnode);
     }
+    
+    T pop()
+    {
+        if (isEmpty())
+            assert(false && "Stack Is Empty");
+            
+        Node *tempnode = topnode;
+        topnode = topnode->next;
 
-    T top() const {
-        if (isEmpty()) cout << ("Stack is empty");
-        return topNode->data;
+        T data = tempnode->data;
+        delete tempnode;
+        return data;
     }
-
-    ~Stack() {
-        while (!isEmpty()) pop();
+    
+    ~stack()
+    {
+        while (!isEmpty())
+            pop();
     }
 };
+// #include <iostream>
+// using namespace std;
+// template <typename T>
+// class Stack {
+// private:
+//     struct Node {
+//         T data;
+//         Node *next;
+//         Node(const T &data, Node *next = nullptr) : data(data), next(next) {}
+//     };
+
+//     Node *topNode;
+
+// public:
+//     Stack() : topNode(nullptr) {}
+
+//     bool isEmpty() const { return topNode == nullptr; }
+
+//     void push(const T &x) {
+//         topNode = new Node(x, topNode);
+//     }
+
+//     void pop() {
+//         if (isEmpty()) return;
+//         Node *oldTop = topNode;
+//         topNode = topNode->next;
+//         delete oldTop;
+//     }
+
+//     T top() const {
+//         if (isEmpty()) cout << ("Stack is empty");
+//         return topNode->data;
+//     }
+
+//     ~Stack() {
+//         while (!isEmpty()) pop();
+//     }
+// };
+
